d8375dded51b1c6506dbaa5ea9c9a32adb50264c
Jihee Kim <jiheekim93@gmail.com>
2022-08-23 22:59:53 -0400

    dummy data = static, added filter but not working yet

--- src/components/DoFilter.js
+++ src/components/DoFilter.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-const DoFilter = () => {
+const DoFilter = (props) => {
   const dropdwonChangeHandler = (event) => {
-    console.log(event.target.value);
+    props.onFilterChange(event.target.value);
   };
 
   return (
     <div>
       <div>
         <label>Filter by year</label>
-        <select onChange={dropdwonChangeHandler}>
+        <select value={props.mainYear} onChange={dropdwonChangeHandler}>
+          <option value="2023">2023</option>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
-          <option value="2019">2019</option>
         </select>
       </div>
     </div>
--- src/components/Dos.js
+++ src/components/Dos.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import DoItem from "./DoItem";
+import DoFilter from "./DoFilter";
+
 const Dos = (props) => {
+  const [year, setYear] = useState("2022");
+
+  const filterChangeHandler = (selectedYear) => {
+    console.log("Dos.js");
+    setYear(selectedYear);
+  };
+
   return (
     <>
+      <DoFilter mainYear={year} onFilterChange={filterChangeHandler} />
       <DoItem title={props.items[0].title} date={props.items[0].date} />
       <DoItem title={props.items[1].title} date={props.items[1].date} />
       <DoItem title={props.items[2].title} date={props.items[2].date} />
